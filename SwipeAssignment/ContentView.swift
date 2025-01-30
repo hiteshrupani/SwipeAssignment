c0e103edec8271cfa85ece1e2e1c2e0baed14142
Hitesh Rupani <rupanihiteshabc@gmail.com>
2025-01-31 00:30:51 +0530

    Initial Commit

---
+++ SwipeAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipeAssignment
+//
+//  Created by Hitesh Rupani on 31/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipeAssignment/SwipeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAssignmentApp.swift
+//  SwipeAssignment
+//
+//  Created by Hitesh Rupani on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
